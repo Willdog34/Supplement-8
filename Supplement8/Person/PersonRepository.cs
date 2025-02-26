9f5ef6d7d20517a992f6d77a9914206836e86f18
Willdog34 <145715999+Willdog34@users.noreply.github.com>
2025-02-25 20:10:54 -0500

    Add files via upload
    This is updated  code for the project

---
+++ Supplement8/Person/PersonRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdvancedLinqAssignment
+{
+    /// <summary>
+    /// Repository for managing and querying a collection of Person objects.
+    /// </summary>
+    public class PersonRepository
+    {
+        private List<Person> data;
+
+        /// <summary>
+        /// Initializes the data field with 1,000,000 unique Person objects.
+        /// </summary>
+        public void InitializeData()
+        {
+            var random = new Random();
+            data = new List<Person>();
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                data.Add(new Person
+                {
+                    Id = i + 1,
+                    Name = $"Person_{random.Next(1, 10000)}",
+                    Birthday = DateTime.Now.AddYears(-random.Next(18, 100))
+                });
+            }
+        }
+
+        /// <summary>
+        /// Returns all people born after the specified date.
+        /// </summary>
+        public IEnumerable<Person> GetPeopleBornAfter(DateTime date)
+        {
+            return data.Where(p => p.Birthday > date);
+        }
+
+        /// <summary>
+        /// Returns all people with the specified name.
+        /// </summary>
+        public IEnumerable<Person> GetPeopleByName(string name)
+        {
+            return data.Where(p => p.Name == name);
+        }
+
+        /// <summary>
+        /// Returns the person with the specified ID, or null if not found.
+        /// </summary>
+        public Person GetPersonById(int id)
+        {
+            return data.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
---
+++ Supplement8/PersonRepoditoryTests/PersonRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using AdvancedLinqAssignment;
+
+[TestClass]
+public class PersonRepositoryTests
+{
+    private PersonRepository repository;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        repository = new PersonRepository();
+        repository.InitializeData();
+    }
+
+    [TestMethod]
+    public void GetPeopleBornAfter_ReturnsCorrectResults()
+    {
+        var date = new DateTime(2000, 1, 1);
+        var result = repository.GetPeopleBornAfter(date);
+        Assert.IsTrue(result.All(p => p.Birthday > date));
+    }
+
+    [TestMethod]
+    public void GetPeopleByName_ReturnsCorrectResults()
+    {
+        var name = "Person_1234";
+        var result = repository.GetPeopleByName(name);
+        Assert.IsTrue(result.All(p => p.Name == name));
+    }
+
+    [TestMethod]
+    public void GetPersonById_ReturnsCorrectPerson()
+    {
+        var id = 42;
+        var result = repository.GetPersonById(id);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(id, result.Id);
+    }
+
+    [TestMethod]
+    public void GetPersonById_ReturnsNullForInvalidId()
+    {
+        var result = repository.GetPersonById(-1);
+        Assert.IsNull(result);
+    }
+}
